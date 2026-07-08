2a84de36205ee7b0ee3e3b56290f0a01be31acfc
HenrySserwadda <sserwaddah12@gmail.com>
2026-07-08 15:07:04 +0300

    day14

---
+++ day13/churn.py
@@ -0,0 +1,6 @@
+#objectives of our project
+#Perform data preprocessing or prepare out data
+
+#1 To find out number of active and inactive customers
+#@ To find out most impactful features in determining customer retention(To find out the distribution of customer characteristics)
+#
